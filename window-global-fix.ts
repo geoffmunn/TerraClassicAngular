b98b00d0553427e7b76f9d9d3df66f7ac6490f5a
Geoff Munn <geoff@wxoutside.tools>
2024-06-10 09:27:48 +1200

    global var polyfill added

---
+++ window-global-fix.ts
@@ -0,0 +1,7 @@
+(window as any).global = window;
+// //(window as any).global.Buffer = require('buffer').Buffer;
+// //(window as any).process = {};
+
+global.Buffer = global.Buffer || require('buffer').Buffer;
+
+console.log ('polyfills loaded')
