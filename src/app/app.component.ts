e552cdfae8aef3c8a8ec51a0cae4bcc6d6d7090b
Geoff Munn <geoff@wxoutside.tools>
2024-06-22 18:07:55 +1200

    app component updated, refactored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { WalletComponent } from './wallet/wallet.component';
+import { BalancesComponent } from './balances/balances.component';
+import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WalletComponent],
+  imports: [RouterOutlet, HomeComponent],
   template: `
-    <router-outlet />
-    <app-wallet></app-wallet>
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
   `,
   styles: [],
 })
