c02a383de1b3c4bbf48b0beae7d04dfd33a14f1f
Geoff Munn <geoff@wxoutside.tools>
2024-06-22 18:07:36 +1200

    Basic wallet list component started

---
+++ src/app/admin/wallet-list/wallet-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wallet-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './wallet-list.component.html',
+  styleUrl: './wallet-list.component.css'
+})
+export class WalletListComponent {
+
+}
