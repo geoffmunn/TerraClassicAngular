e7fe5f4cb73ec2006167344401da6131a5a20f5d
Geoff Munn <geoff@wxoutside.tools>
2024-06-29 16:00:05 +1200

    old code removed

--- window-global-fix.ts
+++ window-global-fix.ts
@@ -1,5 +1,3 @@
 (window as any).global = window;
-// //(window as any).global.Buffer = require('buffer').Buffer;
-// //(window as any).process = {};
 
 global.Buffer = global.Buffer || require('buffer').Buffer;
