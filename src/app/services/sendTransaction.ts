218133d990cbed41df475ee00f806b7670aebc14
Geoff Munn <geoff@wxoutside.tools>
2024-07-17 16:58:26 +1200

    basic transaction service stubs created

---
+++ src/app/services/sendTransaction.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, lastValueFrom, take } from 'rxjs';
+import { Transaction } from './transaction';
+//import { firstValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class sendTransaction extends Transaction {
+
+  constructor() {
+      super();
+  }
+
+//   async getData(uri: string) {
+//     return this.http.get(uri);
+//   }
+
+//   async fetch(uri: string) {
+//     const response:any = await lastValueFrom(await this.getData(uri)).then((name) => {
+//       return name;
+//     })
+
+//     return response.denom_trace.base_denom;
+//   }
+
+//   async getRequest(uri: string) {
+//     var result = await this.fetch(uri);
+    
+//     return result
+//   }
+
+}
+
---
+++ src/app/services/transaction.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, lastValueFrom, take } from 'rxjs';
+//import { firstValueFrom } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class Transaction {
+
+  constructor() {}
+
+}
+
