ae217ecfa2f31edf9682b71f3eb68185c508875c
Geoff Munn <geoff@wxoutside.tools>
2024-07-21 13:26:05 +1200

    send confirmation modal component created

---
+++ src/app/modals/confirm-send/confirm-send.component.ts
@@ -0,0 +1,17 @@
+import { Component, inject, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-confirm-send',
+  standalone: true,
+  imports: [],
+  templateUrl: './confirm-send.component.html',
+  styleUrl: './confirm-send.component.css'
+})
+
+export class ConfirmSendComponent {
+  modal = inject(NgbActiveModal);
+
+  @Input() readable_amount: string = '';
+  @Input() recipient_address: string = '';
+}
