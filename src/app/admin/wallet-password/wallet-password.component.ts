09196a4aec4b5122602f84908e854cfe8ec45f12
Geoff Munn <geoff@wxoutside.tools>
2024-07-21 13:28:06 +1200

    wallet password component upgraded to new system

--- src/app/admin/wallet-password/wallet-password.component.ts
+++ src/app/admin/wallet-password/wallet-password.component.ts
@@ -1,26 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-wallet-password',
-  standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
-  templateUrl: './wallet-password.component.html',
+  selector: 'modal-wallet-password',
+	standalone: true,
+  imports: [FormsModule, ReactiveFormsModule],
+	templateUrl: './wallet-password.component.html',
   styleUrl: './wallet-password.component.css'
 })
-export class WalletPasswordComponent {
-  
-  @Output() decryptionPassword = new EventEmitter();
+
+export class ModalWalletPassword {
+	modal = inject(NgbActiveModal);
 
   walletPassword = new FormGroup({
     walletPassword: new FormControl(''),
   });
-
-  constructor() {}
-
-  decryptWallet() {
-    this.decryptionPassword.emit({'password': this.walletPassword.value.walletPassword});
-  }
-
 }
