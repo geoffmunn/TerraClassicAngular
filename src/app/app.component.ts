c6706d8cc36222e2c17134610cf28efae358cebb
Geoff Munn <geoff@wxoutside.tools>
2024-07-24 22:31:24 +1200

    HttpClient removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+//import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
