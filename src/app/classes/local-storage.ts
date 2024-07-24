a2216ac5745210924117f52b834f4f680665840a
Geoff Munn <geoff@wxoutside.tools>
2024-07-24 22:01:58 +1200

    localStorage changed to class

---
+++ src/app/classes/local-storage.ts
@@ -0,0 +1,17 @@
+export class LocalStorage {
+    public saveData(key: string, value: string) {
+    localStorage.setItem(key, value);
+    }
+
+    public getData(key: string) {
+    return localStorage.getItem(key);
+    }
+    
+    public removeData(key: string) {
+    localStorage.removeItem(key);
+    }
+
+    public clearData() {
+    localStorage.clear();
+    }
+}
