de2885146b04f462dd2d7391c8aaf3e3b7b26548
Geoff Munn <geoff@wxoutside.tools>
2024-07-24 22:31:37 +1200

    unused import removed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { RequestService } from './services/request.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
